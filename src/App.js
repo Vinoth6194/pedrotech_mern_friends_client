22f93d964e4cd0f051a8f269c8fcc947dfce9d6d
Vinoth6194 <49114903+Vinoth6194@users.noreply.github.com>
2020-11-21 01:00:21 +0530

    states and submit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import react, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const addFriend = () => {
+    alert(name + age);
+  };
   return (
     <div className="App">
       {/* Mern Stack Tutorial from PedroTech */}
       <div className="inputs">
-        <input type="text" />
-        <input type="number" />
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <input
+          type="number"
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
+        <button type="submit" onClick={addFriend}>
+          Add Friend
+        </button>
       </div>
     </div>
   );
