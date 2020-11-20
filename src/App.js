e6ce85af1e5ee263372b6528bee1ed7f0d114577
Vinoth6194 <49114903+Vinoth6194@users.noreply.github.com>
2020-11-21 00:42:40 +0530

    input box and styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Mern Stack Tutorial from PedroTech</div>;
+  return (
+    <div className="App">
+      {/* Mern Stack Tutorial from PedroTech */}
+      <div className="inputs">
+        <input type="text" />
+        <input type="number" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
